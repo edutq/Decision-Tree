d4d4094b452266e21bb2ecec3ef2729438f45f12
Lucho Acosta <luiis_acostaa@hotmail.com>
2018-03-24 18:10:17 -0600

    Entropy calculation code and  decision tree algorithm

--- ID3.py
+++ ID3.py
@@ -18,14 +18,45 @@ class Node:
 		self.father = newFather
 
 
-def calculate_entropy(node, dataframe, data_types):
+def calculate_entropy(node, root, data_types):
+	
+	values = []
+	counter = 0
+	entropy = 0.0
+	denominator = len(node.dataframe) - 1
 
+	for element in data_types[root.name]:
+		counter = 0
+		for i in range(1, denominator + 1):
+			if element == node.dataframe[i][root.number]:
+				counter += 1
 
+		if counter > 0:
+			entropy_of_element = - ( (counter/denominator) * math.log(counter/denominator, 2))
+		else:
+			entropy_of_element = 0
+		values.append(entropy_of_element)
+	for element in values:
+		entropy += element
 
-	return nodes
+	return entropy
 
 
 def decision_tree(dataframe, data_types):
+
+	#select the first node to expand
+	#to do this calculate te entropy of each attribuste and choose the one with the entropy closest to 0
+
+	#to calculate the entropy
+		#use the amount of accurrences of each type and placeit on the formula
+		# - type1/totaltypes * log(type1/totaltypes) - type2/totaltypes * log(type2/totaltypes) - typeN/totaltypes * log(typeN/totaltypes)
+		#remeber that the log most be base 2
+
+	#after you selected the node
+		#start the ID3 algorithm with that node
+		#split the node for each value it has 
+		#for each value calculate the GAIN of that attribute
+		#split each of them in decending order based on the info GAIN (this when the recursion happens)
 	
 	return 0
 
@@ -72,3 +103,4 @@ if __name__ == "__main__":
 		print(element)
 	print(data_types)
 	decision_tree(dataframe, data_types)
+	
